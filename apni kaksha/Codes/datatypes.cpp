5de87420bdaea7552c26ef8184f955dfcc4eaa6c
Ananya Ghosh <75447439+A-GHOSH-dev@users.noreply.github.com>
2021-09-24 00:20:32 +0530

    Add files via upload

---
+++ apni kaksha/Codes/datatypes.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a;
+    a=12;
+    cout<<"sizeof int "<<sizeof(a)<<endl;
+    float b;
+        cout<<"sizeof float "<<sizeof(b)<<endl;
+        float b;
+        cout<<"sizeof char "<<sizeof(c)<<endl;
+
+}
